6d5c9a710b84f86d95dfb091ab9a491e093c49a7
Jeremiah <jsnidar@gmail.com>
2021-11-23 13:50:48 -0600

    commit tree f2533876283351701d75012bd250d099d80d2fad

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,24 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+
+  def update
+    plant = Plant.find(params[:id])
+    plant.update(plant_params)
+    render json: plant, except: [:created_at, :updated_at]
+  end
+
+  def destroy
+    plant = Plant.find(params[:id])
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
 end
